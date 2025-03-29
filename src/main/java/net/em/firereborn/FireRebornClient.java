22478cf95de2d008ea6f2dc80bc61491967f85a6
EgorMinechank <egypt_egor@mail.ru>
2025-03-29 12:17:05 +0300

    Initial commit

---
+++ src/main/java/net/em/firereborn/FireRebornClient.java
@@ -0,0 +1,10 @@
+package net.em.firereborn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FireRebornClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/em/firereborn/FireRebornDataGenerator.java
@@ -0,0 +1,11 @@
+package net.em.firereborn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FireRebornDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
