5b2b06b86f97a606ab45fb7d35206b599b356916
EgorMinechank <egypt_egor@mail.ru>
2025-04-05 10:22:03 +0300

    Added understanding event

--- src/main/java/net/em/firereborn/FireReborn.java
+++ src/main/java/net/em/firereborn/FireReborn.java
@@ -7,8 +7,15 @@ import net.em.firereborn.item.ModItems;
 import net.em.firereborn.util.HammerUsageEvent;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonFight;
+import net.minecraft.entity.boss.dragon.EnderDragonPart;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,5 +35,18 @@ public class FireReborn implements ModInitializer {
 		FuelRegistry.INSTANCE.add(ModItems.HOT_COAL, 3200);
 
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+		AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			if(entity instanceof EndermanEntity endermanEntity) {
+				if(player.getMainHandStack().getItem() == ModItems.INFERIUM_HOE) {
+					player.sendMessage(Text.translatable("event.firereborn.livingDamageEvent"));
+					endermanEntity.kill();
+					player.getMainHandStack().decrement(1);
+				}
+
+				return ActionResult.PASS;
+			}
+
+			return ActionResult.PASS;
+		});
 	}
 }
